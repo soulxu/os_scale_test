6f5e1ff7566810d60da8f358df5428d98bcc43ea
He Jie Xu <soulxu@gmail.com>
2020-02-23 17:16:46 -0800

    Fix the run_node.sh

--- run_node.sh
+++ run_node.sh
@@ -3,4 +3,4 @@ HOSTNAME=${2}
 
 NETWORK=bridge
 SCRIPT_DIR="$(pwd)/scripts"
-docker run -d -v "$SCRIPT_DIR:/mnt" --cap-add=NET_ADMIN --privileged --network $NETWORK --hostname $HOSTNAME --name $HOSTNAME -e CONTROLLER_IP=$SERVICE_IP $IMAGE /sbin/init
+docker run -d -v "$SCRIPT_DIR:/mnt" -v /lib/modules/4.4.0-116-generic:/lib/modules/4.4.0-116-generic --cap-add=NET_ADMIN --privileged --network $NETWORK --hostname $HOSTNAME --name $HOSTNAME $IMAGE /sbin/init
